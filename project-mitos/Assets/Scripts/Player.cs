77f86a3ac641962455df05901f44afdcc7f457bd
pratama-aditya-putra <103478726+pratama-aditya-putra@users.noreply.github.com>
2022-05-06 17:55:23 +0700

    tambahan untuk level dungeon

---
+++ project-mitos/Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+    private RaycastHit2D hit;
+    private Vector3 moveDelta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        //Reset Move Delta
+        moveDelta = new Vector3(x,y,0);
+
+        //Rotate sprite to direction
+        if(moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if(moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        //Making sure we can move in this direction, by casting a box there first, if the box return null then we can move 
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0,moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if(hit.collider == null)
+        {
+
+            //Moving sprite
+            transform.Translate(0,moveDelta.y * Time.deltaTime,0);
+
+        }
+        //Making sure we can move in this direction, by casting a box there first, if the box return null then we can move 
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+
+            //Moving sprite
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+
+        }
+
+    }
+}
