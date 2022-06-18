ea2398480d550da63b23b4bae0f5afddd7e5f047
pratama-aditya-putra <103478726+pratama-aditya-putra@users.noreply.github.com>
2022-06-18 15:03:02 +0700

    Update GameManager.cs

--- project-mitos/Assets/Scripts/GameManager.cs
+++ project-mitos/Assets/Scripts/GameManager.cs
@@ -16,6 +16,7 @@ public class GameManager : MonoBehaviour
             Destroy(floatingTextManager.gameObject);
             Destroy(hud);
             Destroy(menu);
+            Destroy(transition);
             return;
         }
         instance = this;
